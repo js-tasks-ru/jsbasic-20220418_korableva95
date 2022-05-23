53a81cb05c5086d3851a6cd96eb69ac41a19f5d9
SoofiaaK <korableva95@yandex.ru>
2022-05-23 08:40:17 +0300

    index,js

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,21 @@
+
 function highlight(table) {
-  // ваш код...
+  let td = table.cells;
+    for (let i =0; i<table.rows.length; i++) {
+      if (td[i].dataset.available === 'true') {
+      tr.classList.toogle('available', true);
+      }
+       else if(td[i].dataset.available === 'false') 
+       {
+          tr.classList.toggle('unavailable', true);
+          } else if (!td[i].hasAttribute('data-available')) {
+            tr.hiden = true;
+             } else if (td[i].textContent === 'm') {
+                tr.classList.toggle('male', true);
+                } else if (td[i].textContent === 'f') {
+                    tr.classList.toggle('female', true);  
+                        } else if (td[i].textContent<18) {
+                      tr.style.textDecoration = 'line-through';
+     }
+   }
 }
