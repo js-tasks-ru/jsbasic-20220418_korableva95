89d26d1b7e8e378099691ee43aabeb577e653f17
SoofiaaK <korableva95@yandex.ru>
2022-05-31 22:02:36 +0300

    index.js

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,42 +1,40 @@
 function initCarousel() {
-  let carousel = document.querySelector('.carousel__inner');
-  let carouselArrowRight = document.querySelector('.carousel__arrow_right');
-  let carouselArrowLeft = document.querySelector('.carousel___arrow_left');
-  let countOfImages = 1;
-  let countClick = 0;
+  let carousel = document.getElementsByClassName('carousel__inner')[0];
+  let carouselArrowRight = document.getElementsByClassName('carousel__arrow_right')[0];
+  let carouselArrowLeft = document.getElementsByClassName('carousel__arrow_left')[0];
+  let countOfImages = document.getElementsByClassName('carousel__slide').length-1; // 3
+  let widthImages = 0;
 
-
-  carouselArrowRight.addEventListener('click', () => {
-    for(let i=0; i>countOfImages.length; i++){
-    if(countOfImages === 4) {
-      carouselArrowRight.style.display = 'none';
-    } 
-    if (countOfImages<4) {
-      carouselArrowRight.style.display = '';
-      carouselArrowLeft.style.display = '';
-      countClick = ++countClick;
-      carousel.style.transform = `translateX(-${carousel.offsetWidth*(countClick)}px)`;
-    }
-    if (countOfImages==1) {
-      carouselArrowLeft.style.display = 'none';
-    }
+  if (widthImages === 0) {
+    carouselArrowLeft.style.display = 'none';
   }
+  
+  carouselArrowRight.addEventListener('click', () => {
+    widthImages -= carousel.offsetWidth;
+
+    carousel.style.transform = `translateX(${widthImages}px)`;
+
+    console.log(widthImages/countOfImages, carousel.offsetWidth);
+    if (widthImages/countOfImages === -carousel.offsetWidth) {
+      carouselArrowRight.style.display = 'none';
+    }
+
+    if (widthImages/countOfImages !== 0) { 
+      carouselArrowLeft.style.display = '';
+    }
   });
   
   carouselArrowLeft.addEventListener('click', () => {
-    for(let i=0; i>countOfImages.length; i++){
-    if(countOfImages === 1) {
-      carouselArrowLeft.style.display = 'none'
-    } 
-    if(countOfImages>1) {
+    widthImages += carousel.offsetWidth;
+
+    carousel.style.transform = `translateX(${widthImages}px)`;
+
+    if (widthImages/countOfImages === 0) { 
+      carouselArrowLeft.style.display = 'none';
+    }
+
+    if (widthImages/countOfImages !== -carousel.offsetWidth) {
       carouselArrowRight.style.display = '';
-      carouselArrowLeft.style.display = '';
-      countClick = --countClick;
-      carousel.style.transform = `translateX(-${carousel.offsetWidth*(countClick)}px)`;
     }
-    if(countOfImages ===4) {
-      carouselArrowRight.style.display = 'none'
-    }
-  }
   });
-  }
+}
